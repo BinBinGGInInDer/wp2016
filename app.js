d7c8b21af5d86e8563c97a2928a5c5a133803abd
BinBinGGInInDer <mxn36@yahoo.com.tw>
2016-10-19 06:49:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 $(function(){
 	var $showImage = $('#show-image');
 	$('.abgne-block a').mouseover(function(){
-		$showImage.attr('src', $(this).attr('href'));
+		var $this = $(this),
+			_src = $this.attr('href');
+		
+		if($showImage.attr('src') != _src){
+			$showImage.hide().attr('src', _src).stop(false, true).fadeTo(400, 1);
+		}
 	}).click(function(){
+		
 		return false;
 	});
 });
+
