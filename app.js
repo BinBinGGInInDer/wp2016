e3917b84a72eeff0f260c58e7f1d4e45ddf1e4e0
BinBinGGInInDer <mxn36@yahoo.com.tw>
2017-01-05 04:50:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+$(doucument).ready(function(){
+  $("#1").fadeIn();
+  $("#2").fadeIn();
+  $("#3").fadeIn();
+  $("#4").fadeIn();
+  $("#5").fadeIn();
+});
+
+
 $(".show").mouseover(function(){
   $("#show-image").attr('src',$(this).attr('src'));
 });
