8219e38a0f51ba4d1cf7f2d36125f79766df215c
BinBinGGInInDer <mxn36@yahoo.com.tw>
2017-01-05 10:11:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,21 +2,12 @@ $(document).ready(init); /*畫面上所有的DOM都載入後*/
 
 
         function init() {
-            /*註冊removeRow函數*/
-            $.fn.removeRow = function (row) {
-                // Make sure row has value     
-               // if (!row) { row = 1; }
-                $(' .css_row :nth-child(' + row + '), .css_row :nth-child(' + row + ')', this).hide();/*該欄隱藏*/
-                return this;
-            };
-            /*註冊addRowl函數*/
-            $.fn.addRow = function (row) {
-                // Make sure col has value     
-                if (!row) { row = 1; }
-                $('tr td:nth-child(' + row + '), tr th:nth-child(' + row + ')', this).show();/*該欄顯示*/
-                return this;
-            };
             
+            $('#R1').hide();
+            $('#R2').hide();
+            $('#R3').hide();
+            $('#R4').hide();
+            $('#R5').hide();
         }
 
 
