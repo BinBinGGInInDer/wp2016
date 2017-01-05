cfdcd6884eb828db48114697b59e4600fbb758a1
BinBinGGInInDer <mxn36@yahoo.com.tw>
2017-01-05 08:50:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-$(doucument).ready(function(){
-  $("#R1").hide();
-  $("#R2").hide();
-  $("#R3").hide();
-  $("#R4").hide();
-  $("#R5").hide();
-});
+$(document).ready(init); /*畫面上所有的DOM都載入後*/
+
+
+        function init() {
+            /*註冊removeRow函數*/
+            $.fn.removeRow = function (row) {
+                // Make sure row has value     
+                if (!row) { row = 1; }
+                $('tr td:nth-child(' + row + '), tr th:nth-child(' + row + ')', this).hide();/*該欄隱藏*/
+                return this;
+            };
+            /*註冊addCol函數*/
+            $.fn.addCol = function (row) {
+                // Make sure col has value     
+                if (!row) { row = 1; }
+                $('tr td:nth-child(' + row + '), tr th:nth-child(' + row + ')', this).show();/*該欄顯示*/
+                return this;
+            };
+            
+        }
 
 
 $(".show").mouseover(function(){
