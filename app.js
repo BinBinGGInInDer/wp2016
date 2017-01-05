6634f932a31d6566b083adbefa338b5192826493
BinBinGGInInDer <mxn36@yahoo.com.tw>
2017-01-05 10:37:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 $(document).ready(init); /*畫面上所有的DOM都載入後*/
-
-
         function init() {
-            
             $('#R1').hide();
             $('#R2').hide();
             $('#R3').hide();
@@ -11,7 +8,22 @@ $(document).ready(init); /*畫面上所有的DOM都載入後*/
         }
 
 
-$(".show").mouseover(function(){
+$("#Sav").onclick(function(){
+        var check=$("#M5").val();
+        if(check==0)
+                alert("超過五個物品囉");
+        else{
+                var T=$("#Projectname").text();
+                $("P5").text(T);
+                T=$("#Unit").text();
+                $("U5").text(T);
+                var N=$("#Num").val();
+                $("N5").val(N);
+                var P=$("#Prc").val();
+                $("P5").val(P);
+                
+        }
+        
   $("#show-image").attr('src',$(this).attr('src'));
 });
 
