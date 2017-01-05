240faef3b8df8357e1782c5dc798eb76bb297686
BinBinGGInInDer <mxn36@yahoo.com.tw>
2017-01-05 10:53:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,26 @@ $(document).ready(init); /*畫面上所有的DOM都載入後*/
 
 
 $("#Sav").onclick(function(){
-        var check=$("#M5").val();
-        if(check==0)
-                alert("超過五個物品囉");
+        var C = $("#Hwmc").val();
+        var check =$("#Projectname").val();
+        if( C ==null || check=="1" )
+                alert("有必填未填");
         else{
-                var T=$("#Projectname").text();
-                $("P5").text(T);
-                T=$("#Unit").text();
-                $("U5").text(T);
-                var N=$("#Num").val();
-                $("N5").val(N);
-                var P=$("#Prc").val();
-                $("P5").val(P);
+                check = $("#M5").val();
+                if( check != 0)
+                        alert("超過五個物品囉");
+                else{
+                        var T=$("#Projectname").text();
+                        $("P5").text(T);
+                        T=$("#Unit").text();
+                        $("U5").text(T);
+                        var N=$("#Num").val();
+                        $("N5").val(N);
+                        var P=$("#Prc").val();
+                        $("P5").val(P);
+                        $("#R5").show();
                 
+                }
         }
         
   $("#show-image").attr('src',$(this).attr('src'));
