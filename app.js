745c8ea27f36a45f821ea05223753985d0100e08
BinBinGGInInDer <mxn36@yahoo.com.tw>
2017-01-05 11:23:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ $(document).ready(function() { /*畫面上所有的DOM都載入後*/
         
 $)('#Sav').click(function() { 
         var C = $("#Hwmc").val();
-        var check =$("#Projectname").val();
-        if( C ==0 || check=="1" )
+        if( C ==0 || document.getElementById('Projectname').value=="請選擇項目名稱" )
                 alert("有必填未填");
         else{
-                check = $("#M5").val();
+                var  check = $("#M5").val();
                 if( check != 0)
                         alert("超過五個物品囉");
                 else{
