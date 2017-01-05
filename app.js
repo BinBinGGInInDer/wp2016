e0bcaed8bdfd9da3a5fa031391bcc55195119890
BinBinGGInInDer <mxn36@yahoo.com.tw>
2017-01-05 08:53:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ $(document).ready(init); /*畫面上所有的DOM都載入後*/
                 $('tr td:nth-child(' + row + '), tr th:nth-child(' + row + ')', this).hide();/*該欄隱藏*/
                 return this;
             };
-            /*註冊addCol函數*/
-            $.fn.addCol = function (row) {
+            /*註冊addRowl函數*/
+            $.fn.addRow = function (row) {
                 // Make sure col has value     
                 if (!row) { row = 1; }
                 $('tr td:nth-child(' + row + '), tr th:nth-child(' + row + ')', this).show();/*該欄顯示*/
